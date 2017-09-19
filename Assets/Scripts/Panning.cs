7e3c369c037e60d80413c08bbfa6f00f8c18a244
Dylan <dylan.ebert@gmail.com>
2017-09-19 16:25:18 -0400

    init

---
+++ Assets/Scripts/Panning.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Panning : MonoBehaviour, IDragHandler {
+
+    public RectTransform container;
+
+    public void OnDrag(PointerEventData data)
+    {        
+        container.Translate(data.delta);
+    }
+}
