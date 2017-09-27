2d3c05673f0692f9b3ffaf01f25dc5e05a7251e7
dylanebert <dylan.ebert@gmail.com>
2017-09-27 12:35:52 -0400

    added scaling with screen size

--- Assets/Scripts/PanelResize.cs
+++ Assets/Scripts/PanelResize.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PanelResize : MonoBehaviour, IDragHandler
 {
     static float MinSize = 256f;
 
     public RectTransform panel;
+    public RectTransform canvasRect;
 
     public void OnDrag(PointerEventData eventData)
     {
-        panel.sizeDelta = new Vector2(Mathf.Max(MinSize, panel.sizeDelta.x + eventData.delta.x), Mathf.Max(MinSize, panel.sizeDelta.y - eventData.delta.y));
+        panel.sizeDelta = new Vector2(Mathf.Max(MinSize, panel.sizeDelta.x + eventData.delta.x / canvasRect.localScale.x), Mathf.Max(MinSize, panel.sizeDelta.y - eventData.delta.y / canvasRect.localScale.y));
     }
 }
